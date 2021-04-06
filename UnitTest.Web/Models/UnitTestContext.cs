c3219aaf0f5cfd338697d55878c55760c5a74722
mertyakdemir <mertyakdemir07@gmail.com>
2021-04-07 01:37:03 +0300

    Add project files.

---
+++ UnitTest.Web/Models/UnitTestContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace UnitTest.Web.Models
+{
+    public partial class UnitTestContext : DbContext
+    {
+        public UnitTestContext()
+        {
+        }
+
+        public UnitTestContext(DbContextOptions<UnitTestContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Turkish_CI_AS");
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Color).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(15, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
